2462de172b96f56c217a2d0745ea4ed1f2918254
Giovanni Montanari <gmontanari@pdxeng.ch>
2022-02-08 17:46:17 +0100

    improve listanozze style

---
+++ src/PaymentOptions/PaymentOptions.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PayementOptions = (p) => {
+    return (
+        <div>
+            <div>
+                Come donare:
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default PayementOptions
