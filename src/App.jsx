0222e2b46cb4a86ae1da1f5168d9a2a411cfb032
Giovanni Montanari <gmontanari@pdxeng.ch>
2022-03-13 19:26:02 +0100

    fix dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Info from './pages/Info/Info'
 import ListaNozze from './pages/ListaNozze/ListaNozze'
 import Menu from './pages/Menu/Menu'
 import Login from './pages/Login/Login'
 import Ricevimento from './pages/Ricevimento/Ricevimento'
-import Loading from './ui_kit/Loading'
 import Page from './ui_kit/Page/Page'
 import { slowly } from './utils'
 
@@ -12,10 +11,16 @@ function App() {
 	const [currentView, setCurrentView] = useState('loading')
 	const goBack = () => slowly(() => setCurrentView('menu'))
 
+	useEffect(() => {
+		const data = localStorage.getItem('user')
+		if (data) setCurrentView('menu')
+		else setCurrentView('login')
+	}, [])
+
 	return (
 		<div className="app_main">
 			<div>
-				{currentView === 'loading' && <Loading setView={setCurrentView} />}
+				{currentView === 'loading' && <div></div>}
 				{currentView === 'login' && <Login setView={setCurrentView} />}
 				{currentView === 'menu' && <Menu setView={setCurrentView} />}
 				{currentView === 'info' && (
