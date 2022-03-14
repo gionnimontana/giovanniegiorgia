43ae8bd032719abd9c3391d6db13f3eb7385b438
Giovanni Montanari <gmontanari@pdxeng.ch>
2022-03-14 23:52:48 +0100

    fix pacchettinozze

--- src/Pages/ListaNozze/pacchettiNozze.js
+++ src/Pages/ListaNozze/pacchettiNozze.js
@@ -3,34 +3,34 @@ const packages = [
 		id: 'V1',
 		img: 'https://www.traveldailymedia.com/assets/2018/11/United-Airlines-Boeing-787-9-Dreamliner-1024x576.jpg',
 		title: 'Voli aerei',
-		text: 'Milano - San Francisco, Los Angeles - Papeete, e volo di ritorno, che lasciamo per ultimo, dovesse mancare ce ne faremo una ragione :)',
+		text: 'Milano - San Francisco, Los Angeles - Papeete. Regalaci un buon viaggio!',
 		purchased: 0
 	},
 	{
 		id: 'V2',
 		img: 'https://www.flightcentre.ca/blog/wp-content/uploads/2016/03/iStock-California-road-trip.jpg',
-		title: 'Noleggio auto e carburante',
-		text: "Visto i recenti aumenti ci serve una sezione dedicata di donazioni, ricordando sempre che 'Se un uomo apre la portiera dell’auto alla moglie, o è nuova l’auto o è nuova la moglie'",
+		title: 'Noleggio auto',
+		text: "Da San Francisco fino al cuore dell'Arizona. 'Se un uomo apre la portiera dell’auto alla moglie, o è nuova l’auto o è nuova la moglie'",
 		purchased: 0
 	},
 	{
 		id: 'V3',
 		img: 'https://qtxasset.com/styles/breakpoint_sm_default_480px_w/s3/fiercepharma/1610037461/GettyImages-1136437406.jpg/GettyImages-1136437406.jpg?VersionId=lKL0To.RG2Hc7qtD3t1UWLR6idJOG5ng&itok=FE3sBy4U',
 		title: 'Big city life - Metropoli Americane',
-		text: 'San Francisco, Los Angeles, Las Vegas. il 10% di quello che donate qui li spenderemo in fast foods, un altro 10% in acquisti stupidi, il resto finiranno tutti su un tavolo da BlackJack!',
+		text: 'San Francisco, Los Angeles, Las Vegas. Il 50% di quello che donate qui li spenderemo in fast foods, il resto finiranno tutti su un tavolo da BlackJack!',
 		purchased: 0
 	},
 	{
 		id: 'V8',
 		img: 'https://pix10.agoda.net/geo/city/3962/1_3962_02.jpg?ca=6&ce=1&s=1920x822',
 		title: 'Giro al parchetto',
-		text: "Sequoia National Park (dove ci sono gli alberi piu' grandi del mondo), Monument Valley, Grand Canyon, Page Canyon, Bryce Canyon. Li acchiapperemo tutti!",
+		text: "Sequoia National Park, Monument Valley, Grand Canyon, Page Canyon, Bryce Canyon. Li acchiapperemo tutti!",
 		purchased: 0
 	},
 	{
 		id: 'V13',
 		img: 'https://www.downunderendeavours.com/wp-content/uploads/2013/11/Moorea-900x600-lodge.jpg',
-		title: 'Una destinazione da sogno',
+		title: 'Polinesia - Una destinazione da sogno',
 		text: "Papeete, Mo'orea, Bora Bora. Le sognamo da una vita e non vediamo l'ora di nuotarci intorno!",
 		purchased: 0
 	},
@@ -38,7 +38,7 @@ const packages = [
 		id: 'V14',
 		img: '/img/bagno-squali-min.jpeg',
 		title: 'Cose che fanno paura a Giorgia',
-		text: 'Un bagno con li squali in polinesia, un esperienza hippie a San Francisco, di sicuro le idee non ci mancano, chissà se ci donerete anche il budget necessario :) ',
+		text: "Un bagno con gli squali in polinesia, un'esperienza hippie a San Francisco, di sicuro le idee non mancano. :) ",
 		purchased: 0
 	},
 	{
@@ -47,7 +47,14 @@ const packages = [
 		title: 'TAMPONI',
 		text: "Perchè dove se il tampone non ce l'hai?",
 		purchased: 0
-	}
+	},
+	{
+		id: 'V17',
+		img: 'https://global-uploads.webflow.com/5fda95cd487da57637f0cbfe/603192dbbbbc28a0aa00c644_air-tahiti-nui-wins-cellars-in-the-sky-gold-medal-dreamliner-over-french-polynesia-24.jpg',
+		title: 'Volo di ritorno',
+		text: "E se dovesse mancare, ce ne faremo una ragione",
+		purchased: 0
+	},
 ]
 
 export default packages
