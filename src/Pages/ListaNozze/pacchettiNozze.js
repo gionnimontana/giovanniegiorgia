12b4358675827b507491a550681c582b0a53f23f
Giovanni Montanari <gmontanari@pdxeng.ch>
2022-03-15 00:04:48 +0100

    add images locally

--- src/Pages/ListaNozze/pacchettiNozze.js
+++ src/Pages/ListaNozze/pacchettiNozze.js
@@ -1,56 +1,56 @@
 const packages = [
 	{
-		id: 'V1',
-		img: 'https://www.traveldailymedia.com/assets/2018/11/United-Airlines-Boeing-787-9-Dreamliner-1024x576.jpg',
+		id: 'voli-aerei',
+		img: '/img/voli-aerei-min.jpeg',
 		title: 'Voli aerei',
 		text: 'Milano - San Francisco, Los Angeles - Papeete. Regalaci un buon viaggio!',
 		purchased: 0
 	},
 	{
-		id: 'V2',
-		img: 'https://www.flightcentre.ca/blog/wp-content/uploads/2016/03/iStock-California-road-trip.jpg',
+		id: 'noleggio-auto',
+		img: '/img/noleggio-auto-min.jpeg',
 		title: 'Noleggio auto',
 		text: "Da San Francisco fino al cuore dell'Arizona. 'Se un uomo apre la portiera dell’auto alla moglie, o è nuova l’auto o è nuova la moglie'",
 		purchased: 0
 	},
 	{
-		id: 'V3',
-		img: 'https://qtxasset.com/styles/breakpoint_sm_default_480px_w/s3/fiercepharma/1610037461/GettyImages-1136437406.jpg/GettyImages-1136437406.jpg?VersionId=lKL0To.RG2Hc7qtD3t1UWLR6idJOG5ng&itok=FE3sBy4U',
+		id: 'big-city-life',
+		img: '/img/big-city-life-min.jpeg',
 		title: 'Big city life - Metropoli Americane',
 		text: 'San Francisco, Los Angeles, Las Vegas. Il 50% di quello che donate qui li spenderemo in fast foods, il resto finiranno tutti su un tavolo da BlackJack!',
 		purchased: 0
 	},
 	{
-		id: 'V8',
-		img: 'https://pix10.agoda.net/geo/city/3962/1_3962_02.jpg?ca=6&ce=1&s=1920x822',
+		id: 'giro-al-parchetto',
+		img: '/img/giro-al-parchetto-min.jpeg',
 		title: 'Giro al parchetto',
 		text: "Sequoia National Park, Monument Valley, Grand Canyon, Page Canyon, Bryce Canyon. Li acchiapperemo tutti!",
 		purchased: 0
 	},
 	{
-		id: 'V13',
-		img: 'https://www.downunderendeavours.com/wp-content/uploads/2013/11/Moorea-900x600-lodge.jpg',
+		id: 'polinesia',
+		img: '/img/polinesia-min.jpeg',
 		title: 'Polinesia - Una destinazione da sogno',
 		text: "Papeete, Mo'orea, Bora Bora. Le sognamo da una vita e non vediamo l'ora di nuotarci intorno!",
 		purchased: 0
 	},
 	{
-		id: 'V14',
+		id: 'bagno-squali',
 		img: '/img/bagno-squali-min.jpeg',
 		title: 'Cose che fanno paura a Giorgia',
 		text: "Un bagno con gli squali in polinesia, un'esperienza hippie a San Francisco, di sicuro le idee non mancano. :) ",
 		purchased: 0
 	},
 	{
-		id: 'V16',
-		img: 'https://immagini.quotidiano.net/?url=http%3A%2F%2Fp1014p.quotidiano.net%3A80%2Fpolopoly_fs%2F1.7220253.1643190294%21%2FhttpImage%2Fimage.jpg_gen%2Fderivatives%2Fwidescreen%2Fimage.jpg&w=660&h=368',
+		id: 'tamponi',
+		img: '/img/tamponi-min.jpeg',
 		title: 'TAMPONI',
 		text: "Perchè dove se il tampone non ce l'hai?",
 		purchased: 0
 	},
 	{
-		id: 'V17',
-		img: 'https://global-uploads.webflow.com/5fda95cd487da57637f0cbfe/603192dbbbbc28a0aa00c644_air-tahiti-nui-wins-cellars-in-the-sky-gold-medal-dreamliner-over-french-polynesia-24.jpg',
+		id: 'voli-di-ritorno',
+		img: '/img/voli-di-ritorno-min.jpeg',
 		title: 'Volo di ritorno',
 		text: "E se dovesse mancare, ce ne faremo una ragione",
 		purchased: 0
