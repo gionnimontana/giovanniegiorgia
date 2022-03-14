eff41fa691b27b56bad6089f525ba8a952f052d5
Giovanni Montanari <gmontanari@pdxeng.ch>
2022-03-14 23:34:53 +0100

    fix pacchetti nozze

--- src/Pages/ListaNozze/pacchettiNozze.js
+++ src/Pages/ListaNozze/pacchettiNozze.js
@@ -2,8 +2,8 @@ const packages = [
 	{
 		id: 'V1',
 		img: 'https://www.traveldailymedia.com/assets/2018/11/United-Airlines-Boeing-787-9-Dreamliner-1024x576.jpg',
-		title: 'Volo Milano - San Francisco',
-		text: 'Regalaci un buon viaggio :)',
+		title: 'Voli aerei',
+		text: 'Milano - San Francisco, Los Angeles - Papeete, e volo di ritorno, che lasciamo per ultimo, dovesse mancare ce ne faremo una ragione :)',
 		purchased: 0
 	},
 	{
@@ -24,42 +24,21 @@ const packages = [
 		id: 'V8',
 		img: 'https://pix10.agoda.net/geo/city/3962/1_3962_02.jpg?ca=6&ce=1&s=1920x822',
 		title: 'Giro al parchetto',
-		text: "Sequoia National Park (dove ci sono gli alberi piu' grandi del mondo), Monument Valley, Grand Canyon, Page Canyon, Bryce Canyon. Gli acchiapperemo tutti!",
-		purchased: 0
-	},
-	{
-		id: 'V11',
-		img: 'https://www.usawelcome.net/kimg/1200/600/7192603hollywood-sign-1200.jpg',
-		title: 'LOS ANGELES',
-		text: '',
-		purchased: 0
-	},
-	{
-		id: 'V12',
-		img: 'https://global-uploads.webflow.com/5fda95cd487da57637f0cbfe/603192dbbbbc28a0aa00c644_air-tahiti-nui-wins-cellars-in-the-sky-gold-medal-dreamliner-over-french-polynesia-24.jpg',
-		title: 'Volo Los Angeles - Papeete',
-		text: '',
+		text: "Sequoia National Park (dove ci sono gli alberi piu' grandi del mondo), Monument Valley, Grand Canyon, Page Canyon, Bryce Canyon. Li acchiapperemo tutti!",
 		purchased: 0
 	},
 	{
 		id: 'V13',
 		img: 'https://www.downunderendeavours.com/wp-content/uploads/2013/11/Moorea-900x600-lodge.jpg',
-		title: '3 notti a MOOREA ',
-		text: 'SOFITEL KIA ORA MOOREA BEACH RESORT',
+		title: 'Una destinazione da sogno',
+		text: "Papeete, Mo'orea, Bora Bora. Le sognamo da una vita e non vediamo l'ora di nuotarci intorno!",
 		purchased: 0
 	},
 	{
 		id: 'V14',
-		img: 'https://i0.wp.com/theamazingraces.com/wp-content/uploads/2018/08/bora-bora-honeymoon.jpg?fit=1600%2C1200&ssl=1',
-		title: '4 notti a BORA BORA',
-		text: 'Bora-Bora Pearl Beach Resort & Spa',
-		purchased: 0
-	},
-	{
-		id: 'V15',
-		img: 'https://www.theartofbusinesstravel.com/wp-content/uploads/2020/05/csm_2382_03_0002short_15a3c8fbe8.jpg',
-		title: 'Volo Papeete-Los Angeles-Francoforte-Milano',
-		text: '',
+		img: '/img/bagno-squali-min.jpeg',
+		title: 'Cose che fanno paura a Giorgia',
+		text: 'Un bagno con li squali in polinesia, un esperienza hippie a San Francisco, di sicuro le idee non ci mancano, chissà se ci donerete anche il budget necessario :) ',
 		purchased: 0
 	},
 	{
