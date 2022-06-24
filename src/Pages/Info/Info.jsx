ffa4809e11df5f9cdc75c325023a58aa2987f4f1
gmontanari <gmontanari@pdxeng.ch>
2022-06-24 16:49:19 +0200

    add indicazioni stradali and parking info

--- src/Pages/Info/Info.jsx
+++ src/Pages/Info/Info.jsx
@@ -17,7 +17,23 @@ const Info = () => {
 			/>
 			<div>Tremezzo, CO</div>
 			<div>Ore 15:30</div>
-
+			<br></br>
+			<div className="info_nocenter">
+				Vicino alla chiesa abbiamo riservato 2 zone parcheggio da 25 posti auto ciascuna:
+			</div>
+			<div className="info_ImageParkingContainer">
+				<img alt="parcheggi" className="info_ImageParking" src="/img/parcheggi.jpg" />
+			</div >
+			<div className="info_parkingButtonContainer">
+				<GMapsButton
+					text="Parcheggio aperto"
+					address="https://goo.gl/maps/1juCyLJRAJBtmmbb8"
+				/>
+				<GMapsButton
+					text="Parcheggio coperto"
+					address="https://goo.gl/maps/BNbTjoB99Eu9U8Gm7"
+				/>
+			</div>
 			<div className="info_ImageLargeContainer">
 				<img alt="foto_villa" className="info_ImageLarge" src="/img/monastero-pax-min.jpeg" />
 			</div>
@@ -27,6 +43,12 @@ const Info = () => {
 				text="Via Antica Regina, Lenno CO"
 				address="https://goo.gl/maps/jcEfqDy7ixkRxzDE6"
 			/>
+			<div className="info_nocenter">
+				Per raggiungere il parcheggio e l'ingresso della location seguire le frecce gialle lungo il percorso:
+			</div>
+			<div className="info_ImageParkingContainer">
+				<img alt="parcheggi" className="info_ImageParking" src="/img/freccia_indicazioni_stradali.jpg" />
+			</div >
 			<div style={{ height: '2rem' }}></div>
 			<div className="info_marginTop info_italic">CONSIGLI UTILI</div>
 			<div className="info_marginTop info_nocenter">
