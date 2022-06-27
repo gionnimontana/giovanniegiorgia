49a1c9336b54e47f168175f13db3446f2c256b8a
Giovanni Montanari <32942764+gionnimontana@users.noreply.github.com>
2022-06-27 14:26:44 +0200

    Update pacchettiNozze.js

--- src/Pages/ListaNozze/pacchettiNozze.js
+++ src/Pages/ListaNozze/pacchettiNozze.js
@@ -4,7 +4,7 @@ const packages = [
 		img: '/img/voli-aerei-min.jpeg',
 		title: 'Voli aerei',
 		text: 'Milano - San Francisco, Los Angeles - Papeete. Regalaci un buon viaggio!',
-		purchased: 3
+		purchased: 4
 	},
 	{
 		id: 'noleggio-auto',
@@ -25,28 +25,28 @@ const packages = [
 		img: '/img/giro-al-parchetto-min.jpeg',
 		title: 'Giro al parchetto',
 		text: 'Sequoia National Park, Monument Valley, Grand Canyon, Page Canyon, Bryce Canyon. Li acchiapperemo tutti!',
-		purchased: 3
+		purchased: 4
 	},
 	{
 		id: 'big-city-life',
 		img: '/img/big-cities-min.jpeg',
 		title: 'Big city life - Metropoli Americane',
 		text: 'Los Angeles e Las Vegas. Il 50% di quello che donate qui li spenderemo in fast foods, il resto finiranno tutti su un tavolo da BlackJack!',
-		purchased: 1
+		purchased: 2
 	},
 	{
 		id: 'polinesia',
 		img: '/img/polinesia-min.jpeg',
 		title: 'Polinesia - Una destinazione da sogno',
 		text: "Papeete, Mo'orea, Bora Bora. Le sognamo da una vita e non vediamo l'ora di nuotarci intorno!",
-		purchased: 4
+		purchased: 5
 	},
 	{
 		id: 'bagno-squali',
 		img: '/img/bagno-squali-min.jpeg',
 		title: 'Cose che fanno paura a Giorgia',
 		text: "Un bagno con gli squali in Polinesia, un'esperienza hippie a San Francisco...",
-		purchased: 4
+		purchased: 5
 	},
 	{
 		id: 'voli-di-ritorno',
